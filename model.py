5d750c1c5e6ca61b8cd0da40279a6b7a982c15db
sayanbanerjee32 <sayanbanerjee32@gmail.com>
2024-02-23 19:35:20 +0530

    Added comments

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# import required packages
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -35,9 +36,7 @@ class NetGAP(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=is_bias)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=is_bias)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, bias=is_bias)
-        # self.ap = nn.AvgPool2d(4)
-        # self.fc1 = nn.Linear(4096, 50)
-        # for global average pool
+
         self.fc1 = nn.Linear(256, 50, bias=is_bias)
         self.fc2 = nn.Linear(50, 10, bias=is_bias)
 
@@ -47,8 +46,9 @@ class NetGAP(nn.Module):
         x = F.relu(self.conv3(x), 2) # 10 x 10 x 128 RF10
         x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 4 x 4 x 256 RF15
         # x = x.view(-1, 4096)
+        # Global average pooling
         x = F.avg_pool2d(x, kernel_size = 4).squeeze()
-        # x = self.ap(x).squeeze()
+
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         # return F.log_softmax(x, dim=1)
@@ -63,10 +63,9 @@ class NetAntmanGAP(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=is_bias)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=is_bias)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, bias=is_bias)
+        ## the antman kernel
         self.antman = nn.Conv2d(256, 10, kernel_size=1, bias=is_bias)
-        # self.ap = nn.AvgPool2d(4)
-        # self.fc1 = nn.Linear(4096, 50)
-        # for global average pool
+
         # self.fc1 = nn.Linear(256, 50)
         # self.fc2 = nn.Linear(50, 10)
 
@@ -76,6 +75,7 @@ class NetAntmanGAP(nn.Module):
         x = F.relu(self.conv3(x), 2) # 10 x 10 x 128
         x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 4 x 4 x 256
         x = self.antman(x) # 4 x 4 x 10
+        # Global average pooling
         x = F.avg_pool2d(x, kernel_size = 4).squeeze()
         # x = self.ap(x).squeeze()
         # x = F.relu(self.fc1(x))
